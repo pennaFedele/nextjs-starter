bcd219e1eb56ed050e00c2519d80db5af7741864
Fedele Penna <157470168+pennaFedele@users.noreply.github.com>
2025-06-09 10:54:11 +0200

    Fix menu UI layout

--- src/app/[lng]/menu/[id]/page.tsx
+++ src/app/[lng]/menu/[id]/page.tsx
@@ -3,18 +3,20 @@ import MenuDetailClient from "@/components/menu-detail-client";
 import { Column, Line, Row, Text } from "@/once-ui/components";
 
 export default async function MenuPage({ params }: { params: { id: string } }) {
-    const { id } = await params;
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/areaCompetenza/${id}?details=true`);
-    const data: ExtendedAreaCompetenza = await response.json();
+  const { id } = await params;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/areaCompetenza/${id}?details=true`,
+  );
+  const data: ExtendedAreaCompetenza = await response.json();
 
-    return (
-        <Column maxWidth="s" gap="16">
-            <Text variant="heading-default-m">{data?.descrizione}</Text>
-            <Text variant="body-default-s">{data?.note}</Text>
-            <Line />
-            <Column fillWidth flex={1} gap="8">
-                <MenuDetailClient menu={data} />
-            </Column>
-        </Column>
-    );
+  return (
+    <Column maxWidth="s" gap="16" flex={1} overflowY="hidden">
+      <Text variant="heading-default-m">{data?.descrizione}</Text>
+      <Text variant="body-default-s">{data?.note}</Text>
+      <Line />
+      <Column fillWidth flex={1} gap="8" overflowY="auto">
+        <MenuDetailClient menu={data} />
+      </Column>
+    </Column>
+  );
 }
--- src/components/pietanza-card.tsx
+++ src/components/pietanza-card.tsx
@@ -1,59 +1,75 @@
 import { Tag } from "@/app/generated/prisma";
 import { ExtendedPietanza } from "@/lib/types";
-import { Card, Column, Media, Row, Text, Line, Icon, Tag as OnceTag } from "@/once-ui/components";
+import { Card, Column, Media, Row, Text, Tag as OnceTag } from "@/once-ui/components";
 
 export default function PietanzaCard({ pietanza }: { pietanza: ExtendedPietanza }) {
+  const nonAllergeneTags = pietanza.tag?.filter((tag: Tag) => !tag.isAllergene);
+  const allergens = pietanza.tag?.filter((tag: Tag) => tag.isAllergene);
 
-    const nonAllergeneTags = pietanza.tag?.filter((tag: Tag) => !tag.isAllergene)
-    const allergens = pietanza.tag?.filter((tag: Tag) => tag.isAllergene)
-    return (
-        <Row  fillWidth>
-            <Card radius="l-4" direction="row" border="neutral-alpha-medium" fillWidth borderStyle="dashed">
-                
-                <Column  fitWidth>
-                    {pietanza.image && (
-                        <Media
-                            border="neutral-alpha-weak"
-                            sizes="120px"
-                            fillWidth
-                            aspectRatio="4 / 3"
-                            radius="l"
-                            alt="Pietanza"
-                            src={pietanza.image}
-                        />
-                    )}
-                </Column>
-                <Column fillWidth paddingX="20" paddingY="24" gap="8">
-                    <Column fillWidth horizontal="space-between" vertical="center">
-                        <Row horizontal="space-between">
-                            <Text variant="body-default-l">{pietanza.descrizioneLingua1}</Text>
-                            <Text variant="label-default-s">{pietanza.prezzo}</Text>
-                        </Row>
-                    </Column>
-                    <Text onBackground="neutral-weak" variant="body-default-s">
-                        <div
-                            dangerouslySetInnerHTML={{
-                                __html: pietanza.note ? pietanza.note : "",
-                            }}
-                        />
-                    </Text>
-                    {allergens && (
-                        <>
-                            <Row
-                                gap="8" vertical="center"
-                                textVariant="label-default-s" onBackground="neutral-medium"
-                            >
-                                {allergens.map((tag: Tag) => (
-                                    <OnceTag key={tag.id} textVariant="label-default-s" >
-                                        {tag.descrizione}
-                                    </OnceTag>
-                                ))}
-                            </Row>
-                        </>
-                    )}
-                </Column>
-
-            </Card>
+  return (
+    <Card
+      radius="l-4"
+      direction="row"
+      border="neutral-alpha-medium"
+      borderStyle="dashed"
+      fillWidth
+      style={{ position: "relative" }}
+    >
+      {nonAllergeneTags && nonAllergeneTags.length > 0 && (
+        <Row gap="4" position="absolute" right="12" top="-12">
+          {nonAllergeneTags.map((tag: Tag) => (
+            <OnceTag key={tag.id} textVariant="label-default-s">
+              {tag.descrizione}
+            </OnceTag>
+          ))}
         </Row>
-    )
+      )}
+
+      <Column flex={1}>
+        {pietanza.image && (
+          <Media
+            border="neutral-alpha-weak"
+            sizes="120px"
+            fillWidth
+            aspectRatio="4 / 3"
+            radius="l"
+            alt="Pietanza"
+            src={pietanza.image}
+          />
+        )}
+      </Column>
+
+      <Column flex={3} paddingX="20" paddingY="24" gap="8">
+        <Row horizontal="space-between" vertical="center">
+          <Text wrap="wrap" variant="body-default-l">
+            {pietanza.descrizioneLingua1}
+          </Text>
+          <Text variant="label-default-s">{pietanza.prezzo}</Text>
+        </Row>
+
+        <Text onBackground="neutral-weak" variant="body-default-s">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: pietanza.note ? pietanza.note : "",
+            }}
+          />
+        </Text>
+
+        {allergens && (
+          <Row
+            gap="8"
+            vertical="center"
+            textVariant="label-default-s"
+            onBackground="neutral-medium"
+          >
+            {allergens.map((tag: Tag) => (
+              <OnceTag key={tag.id} textVariant="label-default-s">
+                {tag.descrizione}
+              </OnceTag>
+            ))}
+          </Row>
+        )}
+      </Column>
+    </Card>
+  );
 }
